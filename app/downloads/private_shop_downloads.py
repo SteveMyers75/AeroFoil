d9d9397e26dabb5eb75842e0a1a6d634b87ec7be
SteveMyers <SteveMyers75@users.noreply.github.com>
2026-05-24 01:09:56 -0600

    Enhance comments in private_shop_downloads.py
    Added comments for clarity on download functionality.

--- app/downloads/private_shop_downloads.py
+++ app/downloads/private_shop_downloads.py
@@ -4,4 +4,6 @@ import time
 
 import requests
 
-# placeholder for Downloading from another AeroFoil server
+# placeholder for Downloading Titles, Updates and DLC from another AeroFoil server
+# using the public GhostShop API and Python scripts
+# Beta test started 05/24/2026@ 01:05 MST
